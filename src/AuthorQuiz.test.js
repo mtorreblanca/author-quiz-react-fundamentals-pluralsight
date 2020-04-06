daec4258d84f16fe2fa0ce59aa96b474ad6ceb15
Marcelo Torreblanca Solís <m_torreblanca@outlook.com>
2020-04-05 21:14:16 -0500

    Test module in events module of the course added, testing mounting, functions and styles of components\

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -1,10 +1,77 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AuthorQuiz from "./AuthorQuiz";
+import Enzyme, { mount, shallow, render } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+Enzyme.configure({ adapter: new Adapter() });
+
+const state = {
+  turnData: {
+    books: ["The Shining"],
+    author: {
+      name: "Charls Dickens",
+      imageUrl: "images",
+      imageSource: "Wikimedia Commons",
+      books: ["David Copperfield", "A Tale of Two Cities"],
+    },
+  },
+  highlight: "none",
+};
 
 describe("Author Quiz", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div");
-    ReactDOM.render(<AuthorQuiz />, div);
+    ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={() => {}} />, div);
+  });
+
+  describe("When no answer has been selected", () => {
+    let wrapper;
+    beforeAll(() => {
+      wrapper = mount(<AuthorQuiz {...state} onAnswerSelected={() => {}} />);
+    });
+
+    it("Should have no background color", () => {
+      expect(wrapper.find("div.row.turn").props().style.backgroundColor).toBe(
+        ""
+      );
+    });
+  });
+
+  describe("When the wrong answer has been selected", () => {
+    let wrapper;
+
+    beforeAll(() => {
+      wrapper = mount(
+        <AuthorQuiz
+          {...Object.assign({}, state, { highlight: "wrong" })}
+          onAnswerSelected={() => {}}
+        />
+      );
+    });
+
+    it("Should have no background color", () => {
+      expect(wrapper.find("div.row.turn").props().style.backgroundColor).toBe(
+        "red"
+      );
+    });
+  });
+
+  describe("When the first answer is selected", () => {
+    let wrapper;
+    const handleAnswerSelected = jest.fn();
+    beforeAll(() => {
+      wrapper = mount(
+        <AuthorQuiz {...state} onAnswerSelected={handleAnswerSelected} />
+      );
+      wrapper.find(".answer").first().simulate("click");
+    });
+
+    it("onAnswerSelected should be called", () => {
+      expect(handleAnswerSelected).toHaveBeenCalled();
+    });
+
+    it("should receive The Shining", () => {
+      expect(handleAnswerSelected).toHaveBeenCalledWith("The Shining");
+    });
   });
 });
