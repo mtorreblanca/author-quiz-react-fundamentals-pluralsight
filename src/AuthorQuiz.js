4d254de1e111716d0766a2eb9f2a22263f864df9
Marcelo Torreblanca Solís <m_torreblanca@outlook.com>
2020-04-05 19:08:54 -0500

    Added onClick handler end of Events Module

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import "./bootstrap.min.css";
+import PropTypes from "prop-types";
 
 const Hero = (props) => {
   return (
@@ -14,29 +15,57 @@ const Hero = (props) => {
   );
 };
 
-const Book = ({ title }) => {
+const Book = ({ title, onClick }) => {
   return (
-    <div className="answer">
+    <div
+      className="answer"
+      onClick={() => {
+        onClick(title);
+      }}
+    >
       <h4>{title}</h4>
     </div>
   );
 };
 
-const Turn = ({ author, books }) => {
+const Turn = ({ author, books, highlight, onAnswerSelected }) => {
+  const highlightToBgColor = (highlight) => {
+    const mapping = {
+      none: "",
+      correct: "green",
+      wrong: "red",
+    };
+    return mapping[highlight];
+  };
+
   return (
-    <div className="row turn" style={{ backgroundColor: "white" }}>
+    <div
+      className="row turn"
+      style={{ backgroundColor: highlightToBgColor(highlight) }}
+    >
       <div className="col-4 offset-1">
         <img src={author.imageUrl} className="authorimage" alt="Athor" />
       </div>
       <div className="col-6">
         {books.map((title) => (
-          <Book title={title} key={title} />
+          <Book title={title} key={title} onClick={onAnswerSelected} />
         ))}
       </div>
     </div>
   );
 };
 
+Turn.propTypes = {
+  author: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+    imageSource: PropTypes.string.isRequired,
+    books: PropTypes.arrayOf(PropTypes.string).isRequired,
+  }),
+  books: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onAnswerSelected: PropTypes.func.isRequired,
+  highlight: PropTypes.string.isRequired,
+};
 const Continue = (props) => {
   return <div></div>;
 };
@@ -52,11 +81,15 @@ const Footer = (props) => {
   );
 };
 
-function AuthorQuiz({ turnData }) {
+function AuthorQuiz({ turnData, highlight, onAnswerSelected }) {
   return (
     <div className="container-fluid">
       <Hero />
-      <Turn {...turnData} />
+      <Turn
+        {...turnData}
+        highlight={highlight}
+        onAnswerSelected={onAnswerSelected}
+      />
       <Continue />
       <Footer />
     </div>
--- src/index.js
+++ src/index.js
@@ -61,15 +61,26 @@ function getTurnData(authors) {
 
 const state = {
   turnData: getTurnData(authors),
+  highlight: "",
 };
 
-ReactDOM.render(
-  <React.StrictMode>
-    <AuthorQuiz {...state} />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+function onAnswerSelected(answer) {
+  const isCorrect = state.turnData.author.books.some((book) => book === answer);
+  console.log("is Correct", isCorrect);
+  state.highlight = isCorrect ? "correct" : "wrong";
+  render();
+}
 
+function render() {
+  ReactDOM.render(
+    <React.StrictMode>
+      <AuthorQuiz {...state} onAnswerSelected={onAnswerSelected} />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+}
+
+render();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
